e27602a6a6cd4c8edcbd84b41989c2c2357cf00f
Jeffrey Tong <trigun0x2@gmail.com>
2013-05-11 13:29:03 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gopexo::Application.config.secret_token = '34d5f94cd28fc4506ee6eab7df5564d6288e427abd607b34e60294281decb06db3f4f72ed9215de623827f66eba3aa7edb2b6a088d5942049a5642cadb8ded1e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Gopexo::Application.config.session_store :cookie_store, key: '_gopexo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Gopexo::Application.config.session_store :active_record_store
