92023a0f84015f2721a1d8f75ad96261de3dd36b
“trigun0x2” <trigun0x2@gmail.com>
2013-06-12 06:04:34 +0000

    scroll to signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Gopexo::Application.routes.draw do
     match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
     match "refer/:token" => "registrations#refer", :as => "referral"
   end
-
+  match '/about' => 'home#about'
   devise_for :users, :controllers => { :registrations => "registrations", :confirmations => "confirmations" }
   match 'users/bulk_invite/:quantity' => 'users#bulk_invite', :via => :get, :as => :bulk_invite
   resources :users do
