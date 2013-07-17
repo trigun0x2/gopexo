854e54930fba5089c8d91e45eeb4b1eab0bb4ed3
Jeffrey Tong <trigun0x2@gmail.com>
2013-07-17 03:38:04 -0400

    mailer changed to info@gopexo.com

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default :from => "jeff@gopexo.com"
+  default :from => "info@gopexo.com"
 
   def welcome_email(user)
     mail(:to => user.email, :subject => "Let's GoPexo!")
